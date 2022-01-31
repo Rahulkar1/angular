a243a9e1bc2ada0da03701a4475dbab823b3e6bd
Rahul <rahulkarthik685@gmail.com>
2022-01-31 16:17:55 +0530

    rahul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { PaymentComponent } from './payment/payment.component';
+import { AddAccountComponent } from './add-account/add-account.component';
+import { BankingComponent } from './banking/banking.component';
+import { CheckBalanceComponent } from './check-balance/check-balance.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveComponent } from './reactive/reactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    PaymentComponent,
+    AddAccountComponent,
+    BankingComponent,
+    CheckBalanceComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
